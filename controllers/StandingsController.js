2fc7ff58e54d51675d2e2bd24c08358150228473
Nick Manton <nick@mantonbradbury.com>
2017-08-30 11:08:11 +0100

    add link to standings in the footer
    remove matches/index link (route is still there,
    but it's not particularly useful)

--- controllers/StandingsController.js
+++ controllers/StandingsController.js
@@ -32,19 +32,12 @@ const controller = {
 
   },
 
-  get_test_id: function(req, res, id) {
-    models.Place.updateTable(id).then(r => {
-      let rr = JSON.stringify(r, null, 2);
-      res.send(`<pre>${rr}</pre>`);
-    })
-  },
-
   get_index: function(req, res) {
     let uid = req.user ? req.user.id : null;
     models.Week.current().then(wk => {
       Promise.join(models.Place.overall(uid, wk.id), models.Standing.overall(uid, wk.id), (tipping, goalmine) => {
         res.render('standings/index', {
-          title: 'Standings at Week ' + wk.id,
+          title: 'Current Standings',
           week: wk.id - 1,
           tipping: tipping,
           goalmine: goalmine
